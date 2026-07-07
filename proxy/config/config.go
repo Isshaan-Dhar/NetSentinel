282d889f69b7a56254b741e225ab0651532989bc
Ishaan Dhar <ishaan.dhar2004@gmail.com>
2026-07-07 16:14:51 +0530

    Add InternalSecret to Config struct
    Added InternalSecret field to Config struct and updated Load function to include it with a default value.

--- proxy/config/config.go
+++ proxy/config/config.go
@@ -5,21 +5,23 @@ import (
 )
 
 type Config struct {
-	AppPort     string
-	PostgresDSN string
-	RedisAddr   string
-	AESKey      string
-	WAFMode     string
-	UpstreamURL string
+	AppPort        string
+	PostgresDSN    string
+	RedisAddr      string
+	AESKey         string
+	WAFMode        string
+	UpstreamURL    string
+	InternalSecret string
 }
 
 func Load() *Config {
 	return &Config{
-		AppPort:     getEnv("APP_PORT", "8080"),
-		PostgresDSN: "postgres://" + getEnv("POSTGRES_USER", "netsentinel") + ":" + getEnv("POSTGRES_PASSWORD", "changeme") + "@" + getEnv("POSTGRES_HOST", "timescaledb") + ":" + getEnv("POSTGRES_PORT", "5432") + "/" + getEnv("POSTGRES_DB", "netsentinel") + "?sslmode=disable",
-		RedisAddr:   getEnv("REDIS_ADDR", "redis:6379"),
-		WAFMode:     getEnv("WAF_MODE", "block"),
-		UpstreamURL: getEnv("UPSTREAM_URL", "http://test-backend:9000"),
+		AppPort:        getEnv("APP_PORT", "8080"),
+		PostgresDSN:    "postgres://" + getEnv("POSTGRES_USER", "netsentinel") + ":" + getEnv("POSTGRES_PASSWORD", "changeme") + "@" + getEnv("POSTGRES_HOST", "timescaledb") + ":" + getEnv("POSTGRES_PORT", "5432") + "/" + getEnv("POSTGRES_DB", "netsentinel") + "?sslmode=disable",
+		RedisAddr:      getEnv("REDIS_ADDR", "redis:6379"),
+		WAFMode:        getEnv("WAF_MODE", "block"),
+		UpstreamURL:    getEnv("UPSTREAM_URL", "http://test-backend:9000"),
+		InternalSecret: getEnv("INTERNAL_API_SECRET", "default-secret"), // SECURITY FIX: Add configurable internal secret
 	}
 }
 
