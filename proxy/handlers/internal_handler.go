cf2ebe9afbd32cda830c9da767cdea23a2ab866a
Ishaan Dhar <ishaan.dhar2004@gmail.com>
2026-07-07 16:09:35 +0530

    Refactor InternalHandler to include database operations
    Modified the InternalHandler to accept a db.Store instance and added functionality to block IPs based on anomaly notifications.

--- proxy/handlers/internal_handler.go
+++ proxy/handlers/internal_handler.go
@@ -1,21 +1,28 @@
 package handlers
 
 import (
+	"context"
 	"encoding/json"
+	"log"
 	"net/http"
+	"time"
 
+	"github.com/isshaan-dhar/NetSentinel/db"
 	"github.com/isshaan-dhar/NetSentinel/metrics"
 )
 
-type InternalHandler struct{}
+type InternalHandler struct {
+	db *db.Store
+}
 
-func NewInternalHandler() *InternalHandler {
-	return &InternalHandler{}
+func NewInternalHandler(store *db.Store) *InternalHandler {
+	return &InternalHandler{db: store}
 }
 
 type anomalyNotification struct {
 	AnomalyType string `json:"anomaly_type"`
 	Severity    string `json:"severity"`
+	ClientIP    string `json:"client_ip"` // Added to receive IP from sidecar
 }
 
 func (h *InternalHandler) RecordAnomaly(w http.ResponseWriter, r *http.Request) {
@@ -25,5 +32,20 @@ func (h *InternalHandler) RecordAnomaly(w http.ResponseWriter, r *http.Request) 
 		return
 	}
 	metrics.AnomaliesDetected.WithLabelValues(n.AnomalyType, n.Severity).Inc()
+
+	// ARCHITECTURAL FIX: Enforce the block directly to the database.
+	if n.ClientIP != "" {
+		go func(ip, anomalyType string) {
+			ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+			defer cancel()
+			err := h.db.BlockIP(ctx, ip, "Behavioral Anomaly: "+anomalyType)
+			if err != nil {
+				log.Printf("failed to block IP %s: %v", ip, err)
+			} else {
+				log.Printf("Successfully blocked IP %s due to %s", ip, anomalyType)
+			}
+		}(n.ClientIP, n.AnomalyType)
+	}
+
 	w.WriteHeader(http.StatusNoContent)
 }
